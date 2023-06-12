839d1c6984e12c144e3f49f25da0fb012a021947
Jonathan Grossman <zelequhen+github@gmail.com>
2023-06-12 14:56:19 -0400

    Create factory for generating random problems and displaying them to the command line

---
+++ src/problem_factory.py
@@ -0,0 +1,8 @@
+import problem
+
+import random
+
+def create_addition(min_operand=0, max_operand=9) -> problem.Problem:
+    op1 = random.randint(min_operand, max_operand)
+    op2 = random.randint(min_operand, max_operand)
+    return problem.Problem(f'{op1}', f'{op2}', '+')
