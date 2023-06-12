2883748dad4802512e13fda459a54d44c04dc696
Jonathan Grossman <zelequhen+github@gmail.com>
2023-06-12 14:56:54 -0400

    Run black across the files

--- src/__main__.py
+++ src/__main__.py
@@ -3,18 +3,28 @@ import argparse
 import problem
 import problem_factory
 
+
 def create_parser() -> argparse.ArgumentParser:
-    parser = argparse.ArgumentParser(prog='Math Worksheet Generator')
-    parser.add_argument('-n', '--num', default=10, type=int, help='The number of math problems to make')
-    subparsers = parser.add_subparsers(dest='problem_type')
-    add_subparser = subparsers.add_parser('add')
-    add_subparser.add_argument('--func', help=argparse.SUPPRESS, default=problem_factory.create_addition)
-    add_subparser.add_argument('-m', '--min', default=0, type=int, help='The minimum value for an operand')
-    add_subparser.add_argument('-M', '--max', default=9, type=int, help='The maximum value for an operand')
+    parser = argparse.ArgumentParser(prog="Math Worksheet Generator")
+    parser.add_argument(
+        "-n", "--num", default=10, type=int, help="The number of math problems to make"
+    )
+    subparsers = parser.add_subparsers(dest="problem_type")
+    add_subparser = subparsers.add_parser("add")
+    add_subparser.add_argument(
+        "--func", help=argparse.SUPPRESS, default=problem_factory.create_addition
+    )
+    add_subparser.add_argument(
+        "-m", "--min", default=0, type=int, help="The minimum value for an operand"
+    )
+    add_subparser.add_argument(
+        "-M", "--max", default=9, type=int, help="The maximum value for an operand"
+    )
 
     return parser
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     parser = create_parser()
     args = parser.parse_args()
 
