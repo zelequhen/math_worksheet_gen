23c0ed6cedbe46f6b06f02dec675c9b4bf5ba33d
Jonathan Grossman <zelequhen+github@gmail.com>
2023-06-12 13:20:14 -0400

    Add validation of operations

--- test/test_problem.py
+++ test/test_problem.py
@@ -24,3 +24,19 @@ class TestProblem(unittest.TestCase):
         ]
         for invalid_operand in INVALID_OPERANDS:
             self.assertFalse(problem.validate_operand(invalid_operand))
+
+    def test_should_detect_valid_operations(self):
+        VALID_OPERATIONS = [
+            '+'
+        ]
+        for operation in VALID_OPERATIONS:
+            self.assertTrue(problem.validate_operation(operation))
+    
+    def test_should_detect_invalid_operations(self):
+        INVALID_OPERATIONS = [
+            None,
+            '',
+            'a'
+        ]
+        for operation in INVALID_OPERATIONS:
+            self.assertFalse(problem.validate_operation(operation))
